1fab1aaacdf223089deb15f3372d353eee4471c2
maibrahi <maibrahi@tcd.ie>
2017-10-19 18:42:28 +0100

    el completo

--- LCATest.py
+++ LCATest.py
@@ -4,7 +4,7 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
 
-    def test(self):
+    def testStandard(self):
         root = LCA.Node(1)
         root.left = LCA.Node(2)
         root.right = LCA.Node(3)
@@ -13,6 +13,26 @@ class MyTestCase(unittest.TestCase):
         root.right.left = LCA.Node(6)
         root.right.right = LCA.Node(7)
         self.assertEqual(2,LCA.findLCA(root, 4, 5, ))
+
+    def test2Nodes(self):
+        root = LCA.Node(1)
+        root.left = LCA.Node(2)
+        self.assertEqual(1,LCA.findLCA(root, 1, 2, ))
+
+
+
+    def testEmpty(self):
+        self.assertEqual(-1,LCA.findLCA(None, None, None, ))
+
+    def testNotPath(self):
+        root = LCA.Node(1)
+        root.left = LCA.Node(2)
+        root.right = LCA.Node(3)
+        root.left.left = LCA.Node(4)
+        self.assertEqual(-1, LCA.findLCA(root, 2, 9, ))
+
+
+
 
 
 
